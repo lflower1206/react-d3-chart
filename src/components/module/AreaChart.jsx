fd5f893f716e1d838622d1488a5beeeccff5aa78
lflower1206 <lflower1206@gmail.com>
2015-08-20 16:13:27 +0800

    add area chart transition

--- src/components/module/AreaChart.jsx
+++ src/components/module/AreaChart.jsx
@@ -9,27 +9,12 @@ var AreaChart = React.createClass({
         height: React.PropTypes.number,
         width: React.PropTypes.number
     },
-    _arcTween: function(d) {
-
-        var that = this,
-            endAngle = Math.min(360 * this.state.ratio, 360) * (Math.PI / 180),
-            interpolate = d3.interpolate(d.endAngle, endAngle);
-        
-        return function(t) {
-            d.endAngle = interpolate(t);
-            
-            return that.state.foregroundArc(d);
-        };
-    },
     _areaTween: function(newData, cb) {
-        console.log(newData);
-        return function(d) {
+        return function(d, i, a) {
         
             var interpolate = d3.interpolateArray(d, newData);
-
+            
             return function(t) {
-                console.log(t);
-                console.log(cb(interpolate(t)));
                 return cb(interpolate(t));
             };
         };
@@ -40,7 +25,7 @@ var AreaChart = React.createClass({
 
         var xScale = d3.scale.ordinal()
                              .domain(d3.range(24))
-                             .rangeRoundBands([0, drawableWidth]);
+                             .rangeBands([0, drawableWidth]);
 
         var yScale = d3.scale.linear()
                              .domain([0, 300])
@@ -82,6 +67,7 @@ var AreaChart = React.createClass({
                              return yScale(d.calories);
                          });
 
+
         var axis = svg.append('g')
                       .attr('transform', 'translate(' + this.state.margin.left + ', ' + this.state.margin.top + ')');
 
@@ -105,30 +91,30 @@ var AreaChart = React.createClass({
     getDefaultProps: function() {
         return {
             data: [
-                {hour: 0, calories: 201},
-                {hour: 1, calories: 154},
-                {hour: 2, calories: 178},
-                {hour: 3, calories: 50},
-                {hour: 4, calories: 68},
-                {hour: 5, calories: 97},
-                {hour: 6, calories: 104},
-                {hour: 7, calories: 111},
-                {hour: 8, calories: 198},
-                {hour: 9, calories: 55},
-                {hour: 10, calories: 76},
-                {hour: 11, calories: 41},
-                {hour: 12, calories: 98},
-                {hour: 13, calories: 174},
-                {hour: 14, calories: 163},
-                {hour: 15, calories: 85},
-                {hour: 16, calories: 91},
-                {hour: 17, calories: 50},
-                {hour: 18, calories: 179},
-                {hour: 19, calories: 181},
-                {hour: 20, calories: 154},
-                {hour: 21, calories: 78},
-                {hour: 22, calories: 81},
-                {hour: 23, calories: 56}
+                {hour: '0', calories: 201},
+                {hour: '1', calories: 154},
+                {hour: '2', calories: 178},
+                {hour: '3', calories: 50},
+                {hour: '4', calories: 68},
+                {hour: '5', calories: 97},
+                {hour: '6', calories: 104},
+                {hour: '7', calories: 111},
+                {hour: '8', calories: 198},
+                {hour: '9', calories: 55},
+                {hour: '10', calories: 76},
+                {hour: '11', calories: 41},
+                {hour: '12', calories: 98},
+                {hour: '13', calories: 174},
+                {hour: '14', calories: 163},
+                {hour: '15', calories: 85},
+                {hour: '16', calories: 91},
+                {hour: '17', calories: 50},
+                {hour: '18', calories: 179},
+                {hour: '19', calories: 181},
+                {hour: '20', calories: 154},
+                {hour: '21', calories: 78},
+                {hour: '22', calories: 81},
+                {hour: '23', calories: 56}
             ],
             height: 320,
             width: 320
